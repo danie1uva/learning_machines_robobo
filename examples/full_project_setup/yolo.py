8387d927195623018101ce6d16e35c09cb4e45f5
EsrionR <86808275+EsrionR@users.noreply.github.com>
2025-01-20 15:32:41 +0100

    image test

--- examples/full_project_setup/yolo.py
+++ examples/full_project_setup/yolo.py
@@ -1,11 +1,16 @@
 from ultralytics import YOLO
-import cv2
 import torch
 
 # Check if GPU is available
 print("CUDA available:", torch.cuda.is_available())
 print("Device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU")
 
-model = YOLO("yolo11x.pt")
-results = model.predict(source = "0", show=True, device = "cuda")
+# Load YOLO model
+model = YOLO("yolo11n.pt")
+
+# Predict on a single image
+image_path = "testimage.jpg"  # Replace with the path to your image
+results = model.predict(source=image_path, show=True, device="cuda")
+
+# Print the results
 print(results)
